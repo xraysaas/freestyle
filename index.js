ae6795624c323cce30a0548cd2dc13531513039d
Ray Xie <Ray.Xie@D2L.com>
2018-11-18 12:21:50 -0800

    fix: yours truly

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let http = require('http');
 
-let server = http.createServer(function(request, response) {
+let server = http.createServer( function( request, response ) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead( 200, { "Content-Type": "text/plain" } );
+    response.end( "Hello World!" );
 
 });
 
 let port = 80;
 server.listen( port );
 
-console.log("Server running at http://localhost:%d", port);
+console.log( "Server running at http://localhost:%d", port );
